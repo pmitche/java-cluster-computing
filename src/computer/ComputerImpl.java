a083ea4c10f724ae8dcdbd05e3e6d0d9dd3e695c
Hajoch <hallvard.jore.christensen@gmail.com>
2015-04-07 19:38:42 -0700

    T

--- src/computer/ComputerImpl.java
+++ src/computer/ComputerImpl.java
@@ -7,8 +7,7 @@ import api.Task;
  * Created by Kyrre on 07.04.2015.
  */
 public class ComputerImpl implements Computer {
-    @Override
-    public T execute(Task task) {
+    public <T> T execute(Task task) {
         return null;
     }
 }
