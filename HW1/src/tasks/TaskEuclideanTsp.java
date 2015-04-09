585c7d1f1ee4c5f9c24fd60e6cf53a4d52c703df
kyrrelm <kyrre.l.moe@gmail.com>
2015-04-08 19:00:55 -0700

    Module

---
+++ HW1/src/tasks/TaskEuclideanTsp.java
@@ -0,0 +1,19 @@
+package tasks;
+
+import api.Task;
+
+import java.util.List;
+
+/**
+ * Created by Kyrre on 08.04.2015.
+ */
+public class TaskEuclideanTsp implements Task<List<Integer>> {
+    public TaskEuclideanTsp(double[][] cities) {
+
+    }
+
+    @Override
+    public List<Integer> execute() {
+        return null;
+    }
+}
