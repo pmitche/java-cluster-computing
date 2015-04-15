6bc718a89f33e234d8b699b3c9fbb996ee0c8622
Paul Mitchell <paulpmit@gmail.com>
2015-04-14 18:23:19 -0700

    Job interface done

---
+++ HW2/src/client/Job.java
@@ -0,0 +1,16 @@
+package client;
+
+import api.Space;
+import api.Task;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by Paul on 14.04.2015.
+ */
+public interface Job<T>{
+
+    public void generateTasks(Space space) throws RemoteException;
+    public T collectResults(Space space) throws RemoteException;
+
+}
