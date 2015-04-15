d7826bc17b827c16baf93315d743e5a9fc1319f2
Hajoch <hallvard.jore.christensen@gmail.com>
2015-04-14 19:29:39 -0700

    EuclideanTsp Job and Task skeleton

---
+++ HW2/src/task/TaskEuclideanTsp.java
@@ -0,0 +1,18 @@
+package task;
+
+import api.Task;
+
+import java.io.Serializable;
+
+/**
+ * Created by Hallvard on 14.04.2015.
+ */
+public class TaskEuclideanTsp implements Serializable, Task{
+
+    public TaskEuclideanTsp(){}
+
+    @Override
+    public Object call() {
+        return null;
+    }
+}
