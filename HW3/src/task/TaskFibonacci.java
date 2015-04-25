9b50b822ff2101c0448b17555b725a4ff3be5259
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2015-04-25 14:26:11 -0700

    task

---
+++ HW3/src/task/TaskFibonacci.java
@@ -0,0 +1,16 @@
+package task;
+
+import javafx.concurrent.Task;
+
+/**
+ * Created by hallvard on 4/25/15.
+ */
+class TaskFibonacci extends Task{
+
+    public TaskFibonacci() {}
+
+    @Override
+    protected Object call() throws Exception {
+        return null;
+    }
+}
