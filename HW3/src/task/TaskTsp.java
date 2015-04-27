ad027c99a035186ef28d3125722fd034ad4cc404
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2015-04-27 15:28:37 -0700

    just trolling the repo..

--- HW3/src/task/TaskTsp.java
+++ HW3/src/task/TaskTsp.java
@@ -6,6 +6,7 @@ import system.CilkThread;
 import system.Closure;
 import system.Continuation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,19 +29,37 @@ public class TaskTsp extends CilkThread implements Task {
 
     @Override
     public void decompose(Continuation c) {
-        int n = c.offset;
+
+        int n = ((Wrapper)c.argument).N;
         if(n < 2) {
             sendArgument(c);
             return;
         }
-        Integer[] a = (Integer[])c.argument;
-        for(int i=1; i<n; i++) {
-            long id = spawnNext()
-            swap(a.clone(), i, n-1);
+        Integer[] a = ((Wrapper)c.argument).PATH;
 
+        //Create all successor lists
+        List<Integer[]> succLists = new ArrayList<>();
+        for(int i=1; i<n; i++)
+            succLists.add(swap(a.clone(), i, n-1));
+
+        //Spawn next to get ID
+        long id = spawnNext(c, new Object[succLists.size()]);
+
+        //Spawn a new Continuation for each successor entry
+        for(int i=0; i<succLists.size(); i++)
+            spawn(new Continuation(id, i+1, new Wrapper(n-1, succLists.get(i))));
+    }
+
+    /**
+     * Private inner class to wrap the two needed values as an single argument
+     */
+    private final static class Wrapper {
+        private final int N;
+        private final Integer[] PATH;
+        private Wrapper(int n, Integer[] path) {
+            this.N = n;
+            this.PATH = path;
         }
-
-
     }
 
     @Override
@@ -53,6 +72,12 @@ public class TaskTsp extends CilkThread implements Task {
 
     }
 
+    /**
+     * Swapping two values
+     * @param a     All values
+     * @param i     to be swapped with i1
+     * @param i1    to be swapped with i
+     */
     private static Integer[] swap(Integer[] a, int i, int i1) {
         assert i>2 && i1> 2;
 
@@ -61,4 +86,33 @@ public class TaskTsp extends CilkThread implements Task {
         a[i1] = tmp;
         return  a;
     }
+
+    /**
+     *
+     * @param d0 is an double[] of length 2, where d0[0] is the x coordinate, and d0[1] is the y coordinates.
+     * @param d1 is an double[] of length 2, where d1[0] is the x coordinate, and d2[1] is the y coordinates.
+     * @return the euclidean distance between do and d1
+     */
+    private double euclideanDistance(double[] d0, double[] d1){
+        double xDistance = Math.abs(d0[0]-d1[0]);
+        double yDistance = Math.abs(d0[1] - d1[1]);
+        return Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+    }
+
+    /**
+     * Calculates the total distance of a given permutation. If the distance is shorter than the current best solution, minDistance and minPath are updated.
+     * @param a
+     *//*
+    private void totalDistance(Integer[] a) {
+        double distance = 0;
+        for (int i = 0; i < a.length; i++){
+            if (i < a.length-1){
+                distance += euclideanDistance(CITIES[a[i]], CITIES[a[i+1]]);
+            }
+        }
+        if (distance < minDistance){
+            minDistance = distance;
+            minPath = a.clone();
+        }
+    }*/
 }
