58fdf6fd980e8b86bb52330b2654050aba1c736e
Paul Mitchell <paulpmit@gmail.com>
2015-04-28 21:29:44 -0700

    Added Javadoc for Continuation

--- HW3/src/system/Continuation.java
+++ HW3/src/system/Continuation.java
@@ -12,16 +12,30 @@ public class Continuation implements Serializable{
     public final Object argument;
     public Object returnVal;
 
+    /**
+     * Continuation constructor
+     * @param closureId: ID of the closure being passed to the constructor
+     * @param offset: Integer offset
+     * @param argument: Object argument being passed to constructor
+     */
     public Continuation(long closureId, int offset, Object argument) {
         this.closureId = closureId;
         this.offset = offset;
         this.argument = argument;
     }
 
+    /**
+     * Sets the return value
+     * @param arg: argument to be returned
+     */
     public void setReturnVal(Object arg) {
         returnVal = arg;
     }
 
+    /**
+     * Getter for returnVal
+     * @return Returns the value
+     */
     public Object getReturnVal() {
         return returnVal;
     }
