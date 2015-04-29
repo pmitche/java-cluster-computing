4ebb6529711070addd0293672fbcbb4d1905e881
ingeboPuzen <oftedal.ingeborg@gmail.com>
2015-04-28 21:12:21 -0700

    javadoc

--- HW3/src/api/Task.java
+++ HW3/src/api/Task.java
@@ -5,10 +5,11 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+/**
+ * interface for decomposing and composing tasks
+ **/
 public interface Task<V> extends Serializable, Callable<V>, Runnable
-{   /**
-    *
-    **/
+{
     @Override
     V call();
 
