40ed56357a78dc656ebf46634a6c08f20bb0e3b4
kyrrelm <kyrre.l.moe@gmail.com>
2015-05-06 18:27:15 -0700

    shit is so async

---
+++ HW4/src/computer/SpaceProxyAsyncHandler.java
@@ -0,0 +1,67 @@
+package computer;
+
+import computer.QueueTicket.Type;
+import space.SpaceImpl;
+import system.Closure;
+import system.Continuation;
+
+import java.rmi.RemoteException;
+import java.util.concurrent.LinkedBlockingQueue;
+import static computer.QueueTicket.Type.*;
+
+/**
+ * Created by Kyrre on 06.05.2015.
+ */
+public class SpaceProxyAsyncHandler implements Runnable {
+
+    private final LinkedBlockingQueue<QueueTicket> waitingQueue;
+
+    public SpaceProxyAsyncHandler(LinkedBlockingQueue<QueueTicket> waitingQueue){
+        this.waitingQueue = waitingQueue;
+    }
+    @Override
+    public void run() {
+        QueueTicket ticket = null;
+        while (true){
+            try {
+                ticket = waitingQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            switch (ticket.getType()){
+                case PUT_CLOSURE:{
+                    try {
+                        SpaceImpl.getInstance().put((Closure) ticket.getArg());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case PUT_CLOSURE_IN_READY:{
+                    try {
+                        SpaceImpl.getInstance().putClosureInReady((Closure) ticket.getArg());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case RECEIVE_ARGUMENT:{
+                    try {
+                        SpaceImpl.getInstance().receiveArgument((Continuation) ticket.getArg());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case CLOSURE_DONE:{
+                    try {
+                        SpaceImpl.getInstance().closureDone((String) ticket.getArg());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
