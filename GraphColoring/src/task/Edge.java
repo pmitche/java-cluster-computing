d1d1f1a97c7d13b81ec8e09605ceb0418600b0a1
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2015-05-27 18:37:34 -0700

    Serilizable and final Edge

--- GraphColoring/src/task/Edge.java
+++ GraphColoring/src/task/Edge.java
@@ -1,12 +1,14 @@
 package task;
 
+import java.io.Serializable;
+
 /**
  * Created by hallvard on 5/26/15.
  */
-public class Edge {
+public class Edge implements Serializable{
 
-    public int id1;
-    public int id2;
+    public final int id1;
+    public final int id2;
 
     public Edge(int id1, int id2) {
         this.id1 = id1;
